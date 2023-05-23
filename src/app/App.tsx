5c3970016ccba40d8a74d3ee1a91d1c0a55d443c
Andrey Chubov <chubov.fortech@gmail.com>
2023-05-23 12:02:54 +0300

    add app route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,7 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProider/lib/useTheme'
 import { MainPage } from 'pages/MainPage'
 import { AboutPage } from 'pages/AboutPage'
+import { AppRouter } from './providers/router'
 
 
 
@@ -16,12 +17,7 @@ export const App = () => {
 				<button onClick={toggleTheme}>TOGGLE</button>
 				<Link to={'/'}>Главная</Link>
 				<Link to={'/about'}>About</Link>
-				<Suspense fallback={<div>Loading...</div>}>
-				<Routes>
-					<Route path={'/about'}  element={<AboutPage/>}/>
-					<Route path={'/'} element={<MainPage/>}/>
-				</Routes>
-			  </Suspense>
+				<AppRouter />
 		
 		</div>
 	)
---
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -0,0 +1,26 @@
+import { AboutPage } from 'pages/AboutPage'
+import { MainPage } from 'pages/MainPage'
+import { RouteProps } from 'react-router-dom'
+
+export enum ApRoutes {
+	MAIN = 'main',
+	ABOUT = 'about',
+}
+
+export const RoutePath: Record<ApRoutes, string> = {
+	[ApRoutes.MAIN]: '/',
+	[ApRoutes.ABOUT]: '/about'
+	
+}
+
+export const routeConfig : Record<ApRoutes, RouteProps> = {
+	[ApRoutes.MAIN]: {
+		path: RoutePath.main,
+		element: <MainPage/>
+	},
+		[ApRoutes.ABOUT]: {
+		path: RoutePath.about,
+		element: <AboutPage/>
+	},
+
+}
