a916a9da0c1b3f1ea6f7ad2f7bbc06e4e0e7d490
Andrey Chubov <chubov.fortech@gmail.com>
2023-08-28 18:22:20 +0300

    change git ignore

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,34 @@
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
+
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  };
+
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff|woff2)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  };
+
+  const cssLoader = buildCssLoader(isDev);
+
+  return [
+    fileLoader,
+    svgLoader,
+    typescriptLoader,
+    cssLoader,
+  ];
+}
