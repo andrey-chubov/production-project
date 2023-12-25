489b83118f3c77f1b80487c0d533318c27c28c13
Andrey Chubov <chubov.fortech@gmail.com>
2023-12-25 10:43:20 +0300

    add test for isolated components with cypress

--- src/shared/lib/tests/ComponentRender/ComponentRender.tsx
+++ src/shared/lib/tests/ComponentRender/ComponentRender.tsx
@@ -6,28 +6,47 @@ import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
+// eslint-disable-next-line andrey-plugin/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
 import i18nForTest from '@/shared/config/i18n/i18nForTest';
+import { Theme } from '@/shared/const/theme';
+// eslint-disable-next-line andrey-plugin/layer-imports
+import '@/app/styles/index.scss';
 
 interface ComponentRenderOptions {
   route?: string;
   initialState?: DeepPartial<StateSchema>;
   asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
+  theme?: Theme;
 }
 
-export const ComponentRender = (component: ReactNode, options: ComponentRenderOptions = {}) => {
+interface TestProviderProps {
+  children: ReactNode;
+  options?: ComponentRenderOptions;
+}
+export function TestProvider(props: TestProviderProps) {
+  const { children, options = {} } = props;
   const {
     route = '/',
     initialState,
     asyncReducers,
+    theme = Theme.LIGHT,
   } = options;
-  render(
-
+  return (
     <MemoryRouter initialEntries={[route]}>
       <StoreProvider asyncReducers={asyncReducers} initialState={initialState as StateSchema}>
         <I18nextProvider i18n={i18nForTest}>
-          {component}
+          <ThemeProvider initialTheme={theme}>
+            <div className={`app ${theme}`}>
+              {children}
+            </div>
+          </ThemeProvider>
         </I18nextProvider>
       </StoreProvider>
-    </MemoryRouter>,
+    </MemoryRouter>
   );
+}
+
+export const ComponentRender = (component: ReactNode, options: ComponentRenderOptions = {}) => {
+  render(<TestProvider options={options}>{component}</TestProvider>);
 };
